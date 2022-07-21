4864a3f1d655db24fe31c3741d9dd9dacccbdae4
danikvh <danivicen@gmail.com>
2022-07-21 13:42:04 +0200

    Operation logic in .js

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
+let calculation;
+let operation;
+let operating = false; //reset display when in an operation
+let first = true; //first iteration of an operation
+let calculated = false; //Pressed the equals button
+
 const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+const equalButton = document.querySelector("#equals");
 
 
 //Listeners
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
         let actualNumber = display.textContent;
-        if (actualNumber === "0") actualNumber = "";
+        if (actualNumber === "0" || operating === true || calculated === true) actualNumber = "";
         actualNumber = actualNumber + button.textContent;
         display.textContent = actualNumber;
+        calculated = false;
     });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click',() => {
+        operation = button.getAttribute("id");
+        if (first) { 
+            calculation = display.textContent;
+            first = false;
+        }
+        else {
+            calculation = operate(eval(operation), 
+            parseInt(calculation), parseInt(display.textContent));
+        }
+        operating = true;
+        display.textContent = calculation;
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    calculation = operate(eval(operation), parseInt(calculation), parseInt(display.textContent));
+    display.textContent = calculation;
+    first = true;
+    operating = false;
+    calculated = true;
 });
 
 
@@ -44,4 +77,3 @@ function operate(operator, x, y) {
     let numbers = new Array(x,y);
     return operator(numbers);
 }
-

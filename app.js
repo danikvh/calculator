74df4350cb4f1674856f84cedd53bef23d3ddcec
danikvh <danivicen@gmail.com>
2022-07-21 12:03:11 +0200

    Added operation functions

---
+++ app.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+	return x + y;
+};
+
+function subtract(x, y) {
+	return x - y;
+};
+
+function multiply(array) {
+    return x * y;
+};
+
+function divide(array) {
+	return x / y;
+};
